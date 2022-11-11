447b329aa7456ad7d38247cebd2dd41c75c71229
Ilango <rajagopalilango@gmail.com>
2022-11-11 13:37:07 +0530

    👌 IMPROVE: Update eslint setup

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,15 +1,4 @@
 module.exports = {
-	extends: [
-		'@remix-run/eslint-config',
-		'@remix-run/eslint-config/node',
-		'prettier',
-	],
-	rules: {
-		'react/prop-types': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'react/display-name': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'@typescript-eslint/no-namespace': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-	},
+	extends: ['@herald/eslint-config'],
+	root: true,
 }
